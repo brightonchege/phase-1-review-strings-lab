9e833b5cc6d426b71b9b5331b93268d391006b6f
brightonchege <brightonspak@gmail.com>
2022-08-15 13:32:55 +0300

    strings complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = "student"
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!` 
+
+
+console.log(welcomeMessage)
+console.log(excitedWelcomeMessage)
+console.log(shortGreeting)
